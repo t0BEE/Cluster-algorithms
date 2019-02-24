b50e878135976050a8fbdde8c0c5fb413a7ac553
Tobias Schiffmann <tobiasschiffm@web.de>
2019-02-24 17:21:46 +0100

    kMenas reorganize

---
+++ Clustering_Algorithms/k-Means/kPoint.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include "kParameter.h"
+
+class Point
+{
+	int clusterNumber;
+protected:
+	double coordinates[DIMENSIONS];
+public:
+	Point();
+	Point(double position[DIMENSIONS]);
+	void setCluster(int);
+	void getCoordinates(double* coordinateBuffer);
+	int getCluster();
+	double getCoordinate(int dimension);
+};
+
+class Centroid: public Point
+{
+public:
+	void setPosition(double position[DIMENSIONS]);
+};
