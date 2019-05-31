927738fc9c7a66d0042ed0fd6f98ef06f85b78fa
t0BEE <tobiasschiffm@web.de>
2019-05-31 08:55:46 +0200

    Remove scripts

--- Clustering_Algorithms/BIRCH/grabData.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-scp it-gold:Studienarbeit/BIRCH_Sequential.csv output/BIRCH_Sequential.csv
-scp it-gold:Studienarbeit/finalOutput.csv output/finalOutput.csv
-scp it-gold:Studienarbeit/outputBIRCH.csv output/outputBIRCH.csv
-scp it-gold:Studienarbeit/BIRCH_times.csv output/BIRCH_times.csv
--- Clustering_Algorithms/BIRCH/prepareServer.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-scp build/BIRCH it-gold:Studienarbeit/BIRCH
-scp measureData.sh it-gold:Studienarbeit/measureData.sh
